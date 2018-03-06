0ddabdbbc5e80f5d4b71f915364ff25d396b107c
George Padolsey <2631084+georgePadolsey@users.noreply.github.com>
2018-03-06 00:51:09 +0000

    Start work on grid / implement basic profile
    Also found good grid manager :D

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,18 +3,19 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import styles from './Home.scss';
 import SidePanel from './SidePanel';
+import MainPanel from './MainPanel';
 import CryptoAPI from '../utils/CryptoAPI';
 
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
-
+  // <div className={styles.container} data-tid="container" />
   render() {
     return (
-      <div>
+      <div className={styles.mainContainer}>
         <SidePanel />
-        <div className={styles.container} data-tid="container" />
+        <MainPanel />
       </div>
     );
   }
--- src/app/components/SidePanel.js
+++ src/app/components/SidePanel.js
@@ -1,7 +1,53 @@
+// @flow
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styles from './SidePanel.scss';
+import moment from 'moment';
+import type { ProfileType } from '../_types/Profile';
+import SpinOnHoverFontAwesome from './SpinOnHoverFontAwesome';
 
-export default class SidePanel extends Component {
+type Props = {
+  profile: ProfileType,
+  lastDataUpdate: ?number
+};
+
+const defaultProfile: ProfileType = {
+  displayName: 'Unnamed User',
+  isReal: false,
+  uuid: '_'
+};
+
+const mapStateToProps = ({ profile, lastDataUpdate }) => {
+  if (profile != null) {
+    return { profile, lastDataUpdate };
+  }
+  return { profile: defaultProfile, lastDataUpdate };
+};
+
+class SidePanel extends Component<Props> {
+  props: Props;
+
   render() {
-    return <div />;
+    return (
+      <div className={styles.sidePanel}>
+        {!this.props.profile.isReal ? (
+          <div className={styles.topContainer}>
+            <div className={styles.profileName}>{this.props.profile.displayName}</div>
+            <span>
+              updated {moment(this.props.lastDataUpdate).fromNow()}{' '}
+              <SpinOnHoverFontAwesome
+                className={styles.refreshButton}
+                name="sync"
+                onClick={() => this.refreshData()}
+              />
+            </span>
+          </div>
+        ) : (
+          <h1>Setup Account</h1>
+        )}
+      </div>
+    );
   }
 }
+
+export default connect(mapStateToProps)(SidePanel);
