e58111da9365841064ac5a805df20a7271b49cac
George Padolsey <2631084+georgePadolsey@users.noreply.github.com>
2018-03-07 09:54:49 +0000

    ConfigFileWork

--- src/app/_app_node/ClientAppReducer.js
+++ src/app/_app_node/ClientAppReducer.js
@@ -1,8 +1,16 @@
 // @flow
 
 import { UPDATE_PROFILE } from '../actions/profile';
+import ProfileProvider from '../nodeComponents/ProfileProvider';
 
-export default async function clientAppReducer(...args) {
-  console.log('IPC recieved', args);
+const profileProvider = new ProfileProvider('config/profiles.json')
+
+export default async function clientAppReducer(action) {
+  console.log(`(${(new Date()).toTimeString()} [ClientAppReducer]: IPC recieved`, args);
+  switch(action.type) {
+    case UPDATE_PROFILE:
+      return [UPDATE_PROFILE,
+        profileProvider.getProfiles()];
+  }
   return [UPDATE_PROFILE, 'cake'];
 }
